5db275c8fb6e2f8d6859461f24f7479853959d25
Kevin Wu <kevwuhoo15@gmail.com>
2023-02-14 16:21:22 -0500

    removed text

--- src/components/ProjectsPage/Projects.js
+++ src/components/ProjectsPage/Projects.js
@@ -19,7 +19,7 @@ const Projects = () => {
             link="https://devpost.com/software/touchless-g72ten"
             name="Touchless"
             type="uOttaHack 5- 2nd Overall, 1st Place Phreesia Challenge, Best Accessbility Hack"
-            desc="When visiting a clinic, two big complaints that we have are the long wait times and the necessity to use a kiosk that thousands of other people have already touched. We also know that certain methods of filling in information are not accessible to everyone (For example, someone with Parkinsons disease writing with a pen). In response to these problems, we created Touchless, an accessible and contact-free solution using voice and touchless gestures to fill out forms."
+            desc="Touchless is an accessible and contact-free solution for gathering form information by allowing users to interact with forms using their voice and gestures."
             techList={[
               "Python",
               "Selenium",
@@ -41,7 +41,7 @@ const Projects = () => {
             link="https://devpost.com/software/sqlidify-10i4zl"
             name="SQLidify"
             type="McHacks 10- 2nd Place Cohere Challenge"
-            desc="SQLidify is an innovative machine learning powered solution to preventing SQL injection exploits in back-end code. SQLidify uses our own unique dataset and training model which consists of over 250 labelled data entries to identify SQL vulnerabilities in an application. To use it, simply paste your code into our website where our machine learning model will identify vulnerabilities in your back-end code, and then will suggest strategies to fix these issues."
+            desc="SQLidify is an innovative machine learning powered solution to preventing SQL injection exploits in back-end code."
             techList={[
               "Python",
               "Flask",
@@ -58,7 +58,7 @@ const Projects = () => {
             link="https://devpost.com/software/streats"
             name="Streats"
             type="DeltaHacks IX"
-            desc="Streats is a web app that allows people to find and share unique street food. Upon accessing the web app, users can view a map which displays markers where other users have found food vendors. Upon discovering a food vendor, users can add it to the map, sharing the type of vendor, food ethnicity, a brief description, and a rating. Upon clicking a marker on the map, the corresponding information will be displayed."
+            desc="Streats is a web app that allows people to find and share unique street food."
             techList={[
               "Node.js",
               "Express.js",
@@ -75,7 +75,7 @@ const Projects = () => {
             link="https://github.com/rejieli/TraQR"
             name="TraQR"
             type="Android App"
-            desc="TraQR is an Android app which allows physical items to be tracked through QR codes. In the app, users can generate a new item instance by providing a name and description. A unique QR code is then generated for the item, and, when scanned using the in-app camera, will display information about the object."
+            desc="TraQR is an Android app which allows physical items to be tracked through QR codes."
             techList={[
               "Java",
               "Firebase",
